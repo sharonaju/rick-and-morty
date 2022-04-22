092c3d94b4e313dc67ea3146e30bdfe9358bfa7f
Sharon <sharonbovas@gmail.com>
2022-04-22 13:02:37 +0400

    Created new Xcode project

---
+++ RickAndMorty/RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Sharon Varghese on 22/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
